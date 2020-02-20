3c19165a9520095e19321c82311a4121e41b34ef
Evan Edwards <39352103+ejedev@users.noreply.github.com>
2020-02-20 11:49:54 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'GrandExchange',
   packages = ['GrandExchange'],
-  version = '0.1',
+  version = '0.1.1',
   license='GPL-3.0',
   description = 'A python wrapper for OSBuddy exchange data.',
   author = 'ejedev',
   author_email = 'ev3098@gmail.com',
   url = 'https://github.com/ejedev/GrandExchange',
-  download_url = 'https://github.com/ejedev/GrandExchange/archive/v0.1.tar.gz',
+  download_url = 'https://github.com/ejedev/GrandExchange/archive/v0.1.1.tar.gz',
   keywords = ['OSRS', 'Oldschool','Runescape','Grand','Exchange','OSBuddy'],
   install_requires=[
     'wheel',
